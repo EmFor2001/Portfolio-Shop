62d361b446f26cfc17742d50b66fe030b16a3fc9
EmFor2001 <k.hajduk.kh@gmail.com>
2021-06-16 13:52:42 +0200

    Pierwsze modyfikacje i nowe komponenty - menu, header, products

---
+++ shop/src/components/ProductsList/PoductsList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Product from './Product';
+
+function ProductsList(props) {
+    return(
+        <div>
+            Products
+            <Product />
+        </div>
+    )
+}
+
+export default ProductsList;
