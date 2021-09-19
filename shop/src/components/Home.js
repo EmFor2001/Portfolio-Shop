389187a7322f767db56e7d7a464bfb50d7f4606c
EmFor2001 <k.hajduk.kh@gmail.com>
2021-09-19 18:11:29 +0200

    Home page with content and style ready to be improved

--- shop/src/components/Home.js
+++ shop/src/components/Home.js
@@ -1,8 +1,20 @@
 const Home = () => {
     return ( 
         <div className="Home">
-            <h2>Home Page</h2>
-        </div>
+                <h2>Home</h2><br />
+                <div className="SectionOne" >
+                    <img src="../img/home1.jpg" alt="no img" className="img1"/>
+                    <p className="text1">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt minima numquam placeat ipsum ad est enim incidunt! Magnam atque, consequuntur commodi nihil odio quaerat architecto a pariatur ex cupiditate officia.</p>
+                </div>
+                <div className="SectionTwo" >
+                    <img src="../img/home2.jpg" alt="no img" className="img2"/>
+                    <p className="text2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque autem quisquam nihil exercitationem hic nulla odit tenetur numquam iure voluptates atque explicabo molestias laudantium, sed eveniet voluptas, quam nemo. Delectus!</p>
+                </div>
+                <div className="SectionThree" >
+                    <img src="../img/home3.jpg" alt="no img" className="img3"/>
+                    <p className="text3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt minima numquam placeat ipsum ad est enim incidunt! Magnam atque, consequuntur commodi nihil odio quaerat architecto a pariatur ex cupiditate officia.</p>
+            </div>
+            </div>
     );
 }
 
